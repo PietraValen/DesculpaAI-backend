51ff0b84a00c552a2ed925768bc938eb98930221
Pietra <pievalentinah@gmail.com>
2026-05-30 11:34:53 -0300

    First Commit: Adiciona estrutura inicial do projeto com Maven, incluindo arquivos de configuração, classes principais e testes

---
+++ src/main/java/com/hackathon/DesculpaAI/DesculpaAiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.DesculpaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesculpaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesculpaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/DesculpaAI/DesculpaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.DesculpaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesculpaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
